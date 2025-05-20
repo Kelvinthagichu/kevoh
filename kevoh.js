af6b8a54b61e1549edd22651c188846cc9d38599
Kelvinthagichu <102993294+Kelvinthagichu@users.noreply.github.com>
2025-05-20 09:20:56 +0300

    Create kevoh.js

---
+++ kevoh.js
@@ -0,0 +1,30 @@
+let name ='23';
+// string not a number
+// primitive datatypes
+// 1.string
+let name3="daniel";
+let name2='daniel';
+// 2. number
+let age2 =20;// number
+//boolean
+let ismarrried=true
+let notmarried=false
+// null< absence of a number
+let x=null;
+//undefined
+let y=undefined;
+// advanced objects
+let a=50;
+let b=20;
+let c=a+b;//70
+let d=a*b//1000
+let k=5%3 //k=>2
+// comparison
+let j=5==5// true
+let l=a>90//false
+let s=!true//false
+let t=!l//true
+// logical operators
+//&&<And>,//<or>
+//or something is true if atleast one thing is true
+let v=false||false||true//true
